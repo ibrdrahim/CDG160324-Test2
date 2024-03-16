f0e2591afdf262679d0af47f0d37d8796f531375
ibrahim <ibrdrahim@gmail.com>
2024-03-16 21:49:40 +0700

    Initial Commit

---
+++ Codigo TMDB/Codigo_TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codigo_TMDBApp.swift
+//  Codigo TMDB
+//
+//  Created by Ibrahim Baisa on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Codigo_TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
